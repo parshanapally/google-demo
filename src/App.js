5bd089146faf35110a5c0ed59d9d504f89f1c84d
Pooja Arshanapally <parshanapally@qca-sdc.com>
2019-02-19 16:14:41 -0600

    feat: add auth store

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
+import { Router, Switch, Route, Redirect } from "react-router-dom";
 
+const SignedOutCallback = () => {
+  return <Redirect to="/" />;
+};
+
+const history = createBrowserHistory();
+const authStore = new AuthStore(history);
+
+const HomeWithAuthStore = () => {
+  return <Home authStore={authStore} />;
+};
 class App extends Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <Router>
+        <Switch>
+          <Route path="/signedout" component={SignedOutCallback} />
+          <Route path="/" component={HomeWithAuthStore} />
+        </Switch>
+      </Router>
+    );
   }
 }
 
