d72c6b0e1f890c8bd14303f01741195f56e1ddc5
Pooja Arshanapally <parshanapally@qca-sdc.com>
2019-02-20 12:23:19 -0600

    fix: fix npm error

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Router, Switch, Route, Redirect } from "react-router-dom";
+import Home from "./components/Home";
+import AuthStore from "./auth-store";
+import createBrowserHistory from "history/createBrowserHistory";
 
+const SignedOutCallback = () => {
+  return <Redirect to="/" />;
+};
+
+const history = createBrowserHistory();
+const authStore = new AuthStore(history);
+
+const HomeWithAuthStore = () => {
+  return <Home authStore={authStore} />;
+};
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/signedout" component={SignedOutCallback} />
+          <Route path="/" component={HomeWithAuthStore} />
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/components/Home.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import Button from "react-bootstrap";
+import { AuthenticatedUserButtons, GuestButton } from "./UserButtons/index";
+import NavHeader from "./NavHeader";
+import { Toaster, Position, Intent } from "@blueprintjs/core";
+
+import { authenticatedCall, noAuthCall } from "../auth-client";
+
+const toaster = Toaster.create({ position: Position.TOP });
+class Home extends Component {
+  state = {
+    authenticated: this.props.authStore.isAuthenticated()
+  };
+
+  handleAuthChange = authenticated => {
+    this.setState({ authenticated: authenticated });
+  };
+  handleAuthentication = () => {
+    if (this.state.authenticated) {
+      return (
+        <AuthenticatedUserButtons signOut={this.props.authStore.signOut} />
+      );
+    } else {
+      return <GuestButton signIn={this.props.authStore.signIn} />;
+    }
+  };
+
+  componentWillMount() {
+    this.props.authStore.subscribe(this.handleAuthChange);
+  }
+  componentWillUnmount() {
+    this.props.authStore.unsubscribe();
+  }
+
+  invokeApi = async call => {
+    try {
+      const response = await call(this.props.authStore);
+      toaster.show({ message: response.message, intent: Intent.SUCCESS });
+    } catch (err) {
+      toaster.show({ message: err.message, intent: Intent.DANGER });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <NavHeader
+          value={this.state.authenticated}
+          handleAuthentication={this.handleAuthentication}
+        />
+        <div className="center">
+          <Button
+            intent="success"
+            text="No Auth"
+            onClick={() => this.invokeApi(noAuthCall)}
+          />
+          <Button
+            intent={this.state.authenticated ? "success" : "danger"}
+            text="Req. Auth"
+            onClick={() => this.invokeApi(authenticatedCall)}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
